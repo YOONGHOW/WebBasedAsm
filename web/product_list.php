6de4d5c97430762891a7e1ebe7b1d490e684cc87
YOONGHOW <yoonghow123@gmail.com>
2024-09-24 17:26:30 +0800

    ewewew
    ew

--- web/product_list.php
+++ web/product_list.php
@@ -9,7 +9,7 @@ $stmt = $_db->prepare("
 
 ?>
  
-<main>
+<main style="min-height: 600px;">
     <p class="introduction">A comprehensive range from electronic devices
          to home appliances are available at here !</p>
 <section>
@@ -36,35 +36,48 @@ $stmt = $_db->prepare("
 
 <?php
     try {
-        if(isset($_POST["search"])){
-            $searchProd = $_POST["search"];
+        if(isset($_POST["search"]) || isset($_POST["category"])){
+            $searchProd = $_POST["search"] ?? '';
+            $selectedCategory = $_POST["category"] ?? 'all';
+        
+            $sql = "
+                SELECT p.*, pi.product_IMG_name, c.*
+                FROM product p
+                LEFT JOIN product_img pi ON p.product_id = pi.product_id
+                LEFT JOIN category c ON p.category_id = c.category_id
+                WHERE p.product_name LIKE :searchProd";
+            
+            if ($selectedCategory != 'all') {
+                $sql .= " AND c.category_name = :selectedCategory";
+            }
+        
+            $stmt = $_db->prepare($sql);
+            $stmt->bindValue(':searchProd', "%$searchProd%");
+            
+            if ($selectedCategory != 'all') {
+                $stmt->bindValue(':selectedCategory', $selectedCategory);
+            }
+        
+        } else {
             $stmt = $_db->prepare("
-            SELECT p.*, pi.product_IMG_name, c.category_name
+            SELECT p.*, pi.product_IMG_name, c.*
             FROM product p
             LEFT JOIN product_img pi ON p.product_id = pi.product_id
             LEFT JOIN category c ON p.category_id = c.category_id
-            WHERE p.product_name LIKE '%$searchProd%'
-
-        "); 
-
-        }else{
-        $stmt = $_db->prepare("
-        SELECT p.*, pi.product_IMG_name, c.category_name
-        FROM product p
-        LEFT JOIN product_img pi ON p.product_id = pi.product_id
-        LEFT JOIN category c ON p.category_id = c.category_id
-        ");
+            ");
         }
-    $stmt->execute();
-    $products = $stmt->fetchAll();
-    if($products == null){
-    echo '<p>No record found(s)</p>';
+
+            $stmt->execute();
+            $products = $stmt->fetchAll();
+            if($products == null){
+            echo '<p>No record found(s)</p>';
     }
 
         foreach ($products as $product) {
             $product_img = "../image/" . $product->product_IMG_name;
     ?>
             <div class="product-item" onclick="window.location.href='product_details.php?product_id=<?= $product->product_id ?>'">
+            <input type="hidden" value="<?=$product->category_name?>"/>
             <input type="hidden" value="<?=$product->product_id?>"/>
             <img src="<?=$product_img ?>" alt="<?=$product->product_name ?>'" class="product-image">
             <h3 class="product-name"><?=$product->product_name ?></h3>
